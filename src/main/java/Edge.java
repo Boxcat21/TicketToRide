48fdc7d89332d6c6a0c706e344cc743fff941a4b
Boxcat21 <42621953+Boxcat21@users.noreply.github.com>
2019-03-26 13:18:39 -0500

    edge

--- src/main/java/Edge.java
+++ src/main/java/Edge.java
@@ -1,4 +1,24 @@
 
 public class Edge {
-
+	private int length;
+	private String color;
+	private Player player;
+	private boolean hasPassed;
+	private City[] cities;
+	
+	public int getLength() {
+		return length;
+	}
+	public String getColor() {
+		return color;
+	}
+	public Player getPlayer() {
+		return player;
+	}
+	public void setPlayer(Player p) {
+		player = p;
+	}
+	public City[] getCities() {
+		return cities;
+	}
 }
