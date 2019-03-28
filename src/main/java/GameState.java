29ea91a716de3dfe8d62076bae72f02772d018b1
AbhinavVarre <37716743+AbhinavVarre@users.noreply.github.com>
2019-03-28 13:37:41 -0500

    constructor

--- src/main/java/GameState.java
+++ src/main/java/GameState.java
@@ -1,5 +1,8 @@
+import java.io.File;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 import java.util.Stack;
 
 public class GameState {
@@ -14,6 +17,43 @@ public class GameState {
 	private ArrayList<City> cities;
 	
 	public GameState() {
+			//Reading in contracts
+			contractList = new LinkedList<>();
+			Scanner scan = new Scanner(new File("tickets.txt"));
+			for(int i =0;i<Integer.parseInt(scan.nextLine());i++)
+			{
+				String[] temp = scan.nextLine().split("|");
+				City one = null;
+				City two = null;
+				for(City c:cities)
+				{
+					if(c.getName().equals(temp[0]))
+					one = c;
+					if(c.getName().equals(temp[1]))
+					two = c;		
+				}
+				contractList.add(new ContractCard(one,two,temp[2]));			
+			}
+			//Writing traincards
+			ArrayList<TrainCard> list = new ArrayList<TrainCard>();
+			trainCardDeck = new Stack();
+			for(int i=0;i<4;i++)
+			{
+				for(int i)
+					
+			}
+			
+			
+					
+				
+			discardTrainCard = new ArrayList<>();
+			//adding players
+			players = new LinkedList<>();
+			players.add(new Player("red"));
+			players.add(new Player("green"));
+			players.add(new Player("yellow"));
+			players.add(new Player("blue"));
+
 		
 	}
 	public Player getCurPlayer() {
