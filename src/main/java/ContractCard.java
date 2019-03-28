2ae49c9174d110e775012066eada0fefdb6c7df9
Colioli <coleguest@comcast.net>
2019-03-28 13:22:48 -0500

    contract card

--- src/main/java/ContractCard.java
+++ src/main/java/ContractCard.java
@@ -1,4 +1,30 @@
 
 public class ContractCard {
 
+	private boolean complete;
+	private City[] cities = new City[2];
+	private int value;
+	
+	public ContractCard(City one, City two, int p)
+	{
+		cities[0] = one;
+		cities[1] = two;
+		value = p;
+	}
+	
+	public boolean isComplete()
+	{
+		return complete;
+	}
+	
+	public int getNumPoints()
+	{
+		return value;
+	}
+	
+	public void complete()
+	{
+		complete = true;
+	}
+	
 }
