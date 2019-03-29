c814e3273d88f963bc2e8ad52ce318568b78b5e0
AbhinavVarre <37716743+AbhinavVarre@users.noreply.github.com>
2019-03-29 13:39:05 -0500

    Update GameState.java

--- src/main/java/GameState.java
+++ src/main/java/GameState.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
@@ -19,6 +21,7 @@ public class GameState {
 	
 	public GameState() throws FileNotFoundException {
 			//Reading in contracts
+			cities = new ArrayList<>();
 			contractList = new LinkedList<>();
 			Scanner scan = new Scanner(new File("tickets.txt"));
 			for(int i =0;i<Integer.parseInt(scan.nextLine());i++)
@@ -35,25 +38,69 @@ public class GameState {
 				}
 				contractList.add(new ContractCard(one,two,Integer.parseInt(temp[2])));			
 			}
-			//Writing traincards
+			//Adding train cards
 			ArrayList<TrainCard> list = new ArrayList<TrainCard>();
 			trainCardDeck = new Stack();
-			for(int i=0;i<4;i++)
+			for(int j=0;j<8;j++)
+			for(int i=0;i<12;i++)
 			{
-				//for(int i)
-					
+				if(j==0)
+					trainCardDeck.add(new TrainCard("Purple"));	
+				else if(j==1)
+					trainCardDeck.add(new TrainCard("White"));	
+				else if(j==2)
+					trainCardDeck.add(new TrainCard("Blue"));
+				else if(j==3)
+					trainCardDeck.add(new TrainCard("Yellow"));	
+				else if(j==4)
+					trainCardDeck.add(new TrainCard("Orange"));	
+				else if(j==5)
+					trainCardDeck.add(new TrainCard("Black"));	
+				else if(j==6)
+					trainCardDeck.add(new TrainCard("Red"));
+				else if(j==7)
+					trainCardDeck.add(new TrainCard("Green"));	
 			}
-			
-			
-					
-				
+			for(int i=0;i<14;i++)
+				trainCardDeck.add(new TrainCard("Wild"));
+			Collections.shuffle(trainCardDeck);
+			//discard list
 			discardTrainCard = new ArrayList<>();
-			//adding players
+			//display cards
+			displayCards = new ArrayList<>();
+			for(int i = 0;i<5;i++)
+			{
+				displayCards.add(trainCardDeck.pop());
+			}
+
+			//adding players, cur player and turncounter
 			players = new LinkedList<>();
 			players.add(new Player("red"));
 			players.add(new Player("green"));
 			players.add(new Player("yellow"));
 			players.add(new Player("blue"));
+			curPlayer = players.peek();
+			turnCounter = 0;
+			//edges and cities 		
+			 scan = new Scanner(new File("ConnectedCities.txt"));
+			 HashMap<String,String> previous = new HashMap<>();
+			 
+			 while(scan.hasNextLine())
+			 {
+				 String[] temp = scan.nextLine().split(",");
+				 if(!((previous.get(temp[1]).equals(temp[0])||(previous.get(temp[0]).equals(temp[1])))))
+					 
+						 
+						 
+				 
+				 
+			 }
+			 
+			 
+			 
+			 
+
+			
 
 		
 	}
@@ -63,27 +110,19 @@ public class GameState {
 	public Queue<Player> getPlayerList() {
 		return players;
 	}
-	public boolean placeTrain(Edge e) {
-		if (!e.getHasTrains()) {
+	public void placeTrain(Edge e) {
+		if (!e.getHasTrains())
 			e.setHasTrains();
 		turnCounter-=2;
-		checkTurn();
-		return true;
-		}
-		return false;
-		
-		
 		
 	}
-	public void chooseTrainCard(int choice) {
+	public boolean chooseTrainCard(int choice) {
 		TrainCard t = displayCards.remove(choice);
 		
 		if(t.getColor().equals("rainbow")) {
 			turnCounter -= 2;
-			if(turnCounter < 0) {
-				turnCounter +=2;
-				return;
-			}
+			if(turnCounter < 0)
+				return false;
 		}
 		else
 			turnCounter--;
@@ -91,33 +130,14 @@ public class GameState {
 		curPlayer.addTrainCard(t);
 		
 		checkTurn();
+		
+		return true;
 	}
 	public void drawTrainCard() {
-		turnCounter -= 2;
-		if(turnCounter < 0) {
-			turnCounter += 2;
-			return;
-		}
-		else
-			turnCounter--;
-		
 		curPlayer.addTrainCard(trainCardDeck.pop());
-		
-		checkTurn();
 	}
 	public String chooseContractCard(ArrayList<Integer> choices) {
-		ContractCard[] cards = new ContractCard[3];
-		
-		cards[0] = contractList.poll();
-		cards[1] = contractList.poll();
-		cards[2] = contractList.poll();
-		
-		for(int i = 0; i < choices.size(); i++)
-		{
-			curPlayer.addContractCard(cards[choices.get(i)]);
-		}
-		
-		return "" + curPlayer.getContracts();
+		return "";
 	}
 	public void endGame() {
 		
