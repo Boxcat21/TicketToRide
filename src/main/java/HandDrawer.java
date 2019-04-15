370aba259ae4a951edcb7a63e032023a7121d8ee
goblinrum <44784975+goblinrum@users.noreply.github.com>
2019-04-15 13:38:22 -0500

    yeet

--- src/main/java/HandDrawer.java
+++ src/main/java/HandDrawer.java
@@ -1,3 +1,4 @@
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -15,22 +16,44 @@ public class HandDrawer {
 	public static void drawHand(Graphics g, Player p) {
 		Graphics2D g2 = (Graphics2D) g;
 		g2.drawRect(0,	 720, 1440, 360);
-		g2.setColor(Color.LIGHT_GRAY);
+		g2.setColor(new Color(255,0,255));
 		g2.fillRect(0, 721, 1440, 360);
 		ArrayList<TrainCard> trainCard = p.getTrainCards();
 		ArrayList<ContractCard> contracts = p.getContracts();
 		g2.setColor(Color.BLACK);
 		int x = 60;
 		for ( int i = 0; i < COLOR_NAMES.length; i++) {
+			
 			g2.setColor(Color.BLACK);
-			g2.drawRect(x, 900, 60, 180);
+			g2.setStroke(new BasicStroke(5));
+			g2.drawRect(x, 900, 30, 180);
+			g2.drawRect(x+30, 900, 30, 180);
+			g2.drawLine(x, 940, x+60, 940);
+			g2.setStroke(new BasicStroke(1));
+			g2.setColor(Color.WHITE);
+			g2.fillRect(x, 900, 30, 180);
+			//g2.setColor(Color.WHITE);
+			g2.fillRect(x+30, 900, 30, 180);
 			g2.setColor(COLOR_NAMES[i]);
 			g2.fillRect(x, 940, 60, 120);
+			if ( TRAIN_COLORS[i].equals("Black"))
+				g2.setColor(Color.WHITE);
+			else
+				g2.setColor(Color.BLACK);
+			g2.drawString(getCount(TRAIN_COLORS[i], trainCard), x+28, 1010);
 			x+=120;
 		}
 		
 		
 		
+		
+	}
+	private static String getCount(String string, ArrayList<TrainCard> list) {
+		int count = 0;
+		for ( TrainCard c : list )
+			if ( c.getColor().equals(string))
+				count++;
+		return ""+count;
 	}
 	
 	
