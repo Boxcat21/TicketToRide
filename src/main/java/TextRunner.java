f8e2eef36e6d49f8b65b8d285a4894087a5a0349
AbhinavVarre <37716743+AbhinavVarre@users.noreply.github.com>
2019-04-15 22:29:07 -0500

    textruner

--- src/main/java/TextRunner.java
+++ src/main/java/TextRunner.java
@@ -1,10 +1,32 @@
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class TextRunner {
+	static GameState g;
 	public static void main(String[]args) throws FileNotFoundException 
 	{
 		//Instantiating the game
-		GameState g = new GameState();
+		 g = new GameState();
+		//Going through the game
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter d to draw cards, enter c to draw contracts, enter p to place train");
+		if(scan.nextLine().equals("d"))
+		{
+			testDrawTrainCards();
+		}
 	}
+	public static void testDrawTrainCards()
+	{
+	 
+	}
+	public static void testDrawContract()
+	{
+		
+	}
+	public static void testPlaceTrains()
+	{
+		
+	}
+	
 
 }
