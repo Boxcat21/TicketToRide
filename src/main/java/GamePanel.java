787dcfc55b44b29e3874f8328b9ba802fcd87799
goblinrum <44784975+goblinrum@users.noreply.github.com>
2019-05-15 08:13:05 -0500

    Update GamePanel.java

--- src/main/java/GamePanel.java
+++ src/main/java/GamePanel.java
@@ -119,7 +119,7 @@ public class GamePanel extends JPanel implements MouseListener {
 				g.drawString("Place: " + (i+1)  + "    " + end.get(i), 940, 400 + (i*30));
 			}
 			g.drawString(game.getLongestPath(), 940, 700);
-			g.drawString("MOST CONTRACTS" + game.getMostContracts().getTrainColor() + " | " + game.getMostContracts().getCompleted()  + " contracts", 940, 740);
+			g.drawString("MOST CONTRACTS: " + game.getMostContracts().getTrainColor() + " | " + game.getMostContracts().getCompleted()  + " contracts", 940, 740);
 		}
 		/*
 		 * BoardDrawer.drawBoard(g, this.clickedEdgeIndecies); HandDrawer.drawHand(g,
